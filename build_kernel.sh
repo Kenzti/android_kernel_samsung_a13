14a12a5ab895a37cc3aac22696a4c5b4d34459e3
Kenzti <135243590+Kenzti@users.noreply.github.com>
2026-07-22 13:35:48 +0700

    Refactor build_kernel.sh to streamline kernel build process
    Removed the build_kernel function and integrated its logic directly into the script.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
 
-make clean && make mrproper
+DIR="$(readlink -f .)"
+PARENT_DIR="$(readlink -f "${DIR}/..")"
+
+export CROSS_COMPILE="$PARENT_DIR/clang-r450784d/bin/aarch64-linux-gnu-"
+export CC="$PARENT_DIR/clang-r450784d/bin/clang"
+export PATH="$PARENT_DIR/clang-r450784d/bin:$PATH"
+export LLVM_LDFLAGS="-fuse-ld=mold"
+export LD="mold"
+export LLVM=1
+export ARCH=arm64
+export PLATFORM_VERSION=13
 
 export KERNEL_ROOT="$(pwd)"
-export PATH="$(readlink -f "${KERNEL_ROOT}/../clang-r450784d/bin"):${PATH}"
 
-# Build options for the kernel
-export BUILD_OPTIONS="
+BUILD_OPTIONS="
 ARCH=arm64 \
 LLVM=1 \
+LD=mold \
+LLVM_LDFLAGS=-fuse-ld=mold \
+CROSS_COMPILE=$PARENT_DIR/clang-r450784d/bin/aarch64-linux-gnu- \
+CC=$PARENT_DIR/clang-r450784d/bin/clang \
 KCFLAGS=-w \
-PLATFORM_VERSION=14 \
-ANDROID_MAJOR_VERSION=14 \
 KBUILD_BUILD_USER=ken \
 KBUILD_BUILD_HOST=pkenti
 "
 
-build_kernel(){
-    # Make default configuration.
-    make ${BUILD_OPTIONS} -j$(nproc --all) exynos850-a13xx_defconfig
-    
-    # make ${BUILD_OPTIONS} -j$(nproc --all) menuconfig
-
-    # Build the kernel
-    make ${BUILD_OPTIONS} -j$(nproc --all) Image || exit 1
-
-    # Copy the built kernel to the build directory
-    if [ -e ${KERNEL_ROOT}/arch/arm64/boot/Image ]; then
-    cp ${KERNEL_ROOT}/arch/arm64/boot/Image AnyKernel3
-
-    echo "[+] Packaging kernel ..."
-    cd AnyKernel3 && rm -f *.zip
-    zip -r9 A135F-KVM.zip *
-    echo "[-] Package done!"
-    else
-        exit 1
-    fi
-}
-build_kernel
+make ${BUILD_OPTIONS} -j$(nproc --all) exynos850-a13xx_defconfig
+make ${BUILD_OPTIONS} -j$(nproc --all) Image || exit 1
