5da305f4008c7c1183d7fdefbb30720928b45705
Kenzti <135243590+Kenzti@users.noreply.github.com>
2026-07-22 13:13:08 +0700

    Enhance kernel build script with options and packaging
    Added build options and packaging steps for kernel.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
-export ARCH=arm64
-export PLATFORM_VERSION=13
+make clean && make mrproper
 
-make ARCH=arm64 exynos850-a13xx_defconfig
-make ARCH=arm64 -j16
+export KERNEL_ROOT="$(pwd)"
+
+# Build options for the kernel
+export BUILD_OPTIONS="
+ARCH=arm64 \
+LLVM=1 \
+LLVM_IAS=1 \
+KCFLAGS=-w \
+PLATFORM_VERSION=14 \
+ANDROID_MAJOR_VERSION=14 \
+KBUILD_BUILD_USER=ken \
+KBUILD_BUILD_HOST=pkenti
+"
+
+build_kernel(){
+    # Make default configuration.
+    make ${BUILD_OPTIONS} -j$(nproc --all) exynos850-a13xx_defconfig
+    
+    # make ${BUILD_OPTIONS} -j$(nproc --all) menuconfig
+
+    # Build the kernel
+    make ${BUILD_OPTIONS} -j$(nproc --all) Image || exit 1
+
+    # Copy the built kernel to the build directory
+    if [ -e ${KERNEL_ROOT}/arch/arm64/boot/Image ]; then
+    cp ${KERNEL_ROOT}/arch/arm64/boot/Image AnyKernel3
+
+    echo "[+] Packaging kernel ..."
+    cd AnyKernel3 && rm -f *.zip
+    zip -r9 A135F-KVM.zip *
+    echo "[-] Package done!"
+    else
+        exit 1
+    fi
+}
+build_kernel
